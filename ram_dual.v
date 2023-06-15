8135a27eb4c5af1e6134cf795f308cf05d9fbea8
ANKITASO <126142577+ANKITASO@users.noreply.github.com>
2023-06-15 14:18:21 +0530

    Dual Port Block RAM

---
+++ ram_dual.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.04.2023 16:52:57
+// Design Name: 
+// Module Name: ram_dual
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ram_dual(
+    input clk
+    );
+endmodule
