11bf8e329dc20e5a40d5535af4ce4d08a2b7eb24
ANKITASO <126142577+ANKITASO@users.noreply.github.com>
2023-06-15 14:19:36 +0530

    RAM dual port

--- ram_dual.v
+++ ram_dual.v
@@ -1,26 +1,40 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.04.2023 16:52:57
-// Design Name: 
-// Module Name: ram_dual
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ram_dual(
-    input clk
+    input clka, clkb,
+    input  ena, enb, wr,
+    input [7:0] adda,
+    input [7:0] addb,
+    input [7:0] in_a,
+    input [7:0] in_b,
+    output reg [7:0] out_a,
+    output reg [7:0] out_b
     );
+reg [7:0] mema [255:0];
+reg [7:0] memb [255:0];
+always @(posedge clka)
+begin
+  if (ena)
+   begin
+     if (wr) begin
+       mema[adda] <= in_a;
+     end
+     else begin
+      out_a <= mema[adda];
+     end
+  end
+end
+
+always @(posedge clkb)
+begin
+  if (enb)
+   begin
+     if (wr) begin
+       memb[addb] <= in_b;
+     end
+     else begin
+      out_b <= memb[addb];
+     end
+  end
+end
 endmodule
